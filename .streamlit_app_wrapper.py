c7d701de8f1292252307933f53b34884a402e2f9
mgm67671 <mightymatt67@gmail.com>
2025-04-05 10:18:41 -0500

    removed extra file

--- .streamlit_app_wrapper.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-# Ensure the root of the project is in the path
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-
-# Import and run the app
-import frontend.streamlit_app
